a40e3ba91e397d9c31880b1ef3ec9cadb57de1b1
Ole Thomas Tallerås <olethomas@dekode.no>
2020-09-02 23:16:23 +0200

    Add SearchWP 4.0 support for stopwords

--- theme-supports/searchwp/norwegian-stopwords.php
+++ theme-supports/searchwp/norwegian-stopwords.php
@@ -15,9 +15,12 @@ namespace Dekode\Resets\ThemeSupports\SearchWP\StopWords;
  */
 function add_norwegian_stopwords( array $terms ) : array {
 	if ( file_exists( __DIR__ . '/stopwords-nb_NO.txt' ) ) {
-		$terms = explode( PHP_EOL, file_get_contents( __DIR__ . '/stopwords-nb_NO.txt' ) );
+		$terms = explode( PHP_EOL, trim( file_get_contents( __DIR__ . '/stopwords-nb_NO.txt' ) ) );
 	}
 
 	return $terms;
 }
+// SearchWP < 4
 add_filter( 'searchwp_stopwords', __NAMESPACE__ . '\\add_norwegian_stopwords' );
+// SearchWP > 4
+add_filter( 'searchwp\stopwords', __NAMESPACE__ . '\\add_norwegian_stopwords' );
