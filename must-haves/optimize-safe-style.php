b987171e99d16925fb0604ea3241dc5f0f9de7d9
Ole Thomas Tallerås <olethomas@dekode.no>
2020-11-24 10:35:12 +0100

    Refactor namespace and filename

--- must-haves/optimize-safe-style.php
+++ must-haves/optimize-safe-style.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * Optimize formats
+ * Optimize safe style css
  *
  * @package Dekode\Resets
  */
 
 declare( strict_types = 1 );
-namespace Dekode\Resets\ThemeSupports\Formats;
+namespace Dekode\Resets\MustHaves\SafeStyle;
 
 /*
  * Allow object-position in style attribute.
  */
 add_filter( 'safe_style_css', function( array $styles ): array {
 	$styles[] = 'object-position';
+
 	return $styles;
 } );
